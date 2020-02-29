a872a97a3b7c225300b7de388d9c0f01187f03b0
eta aoki <imposter.kore2626@gmail.com>
2020-02-29 19:36:11 +0900

    [add] genarate shell & docker-compose.yaml

---
+++ openapi-gen.sh
@@ -0,0 +1,48 @@
+#! bin/bash
+
+inOpt_Key=$1
+inOpt_Val=$2
+genOpt_Key=$3
+genOpt_Val=$4
+outOpt_Key=$5
+outOpt_Val=$6
+
+help="openapi-gen.sh -i <import.file> -g <generate type> -o <output path>"
+
+# -i option check
+if [ -z ${inOpt_Key} ] || [ ${inOpt_Key} != "-i" ]; then
+    echo "[EROR] Specify '-i' as the first argument."
+    echo "      " ${help}
+    exit 1
+fi
+if [ -z ${inOpt_Val} ]; then
+    echo "[EROR] No value has been set for the '-i' option."
+    echo "      " ${help}
+    exit 1
+fi
+
+# -g option check
+if [ -z ${genOpt_Key} ] || [ ${genOpt_Key} != "-g" ]; then
+    echo "[EROR] Specify '-g' as the second argument."
+    echo "      " ${help}
+    exit 1
+fi
+if [ -z ${genOpt_Val} ]; then
+    echo "[EROR] No value has been set for the '-g' option."
+    echo "      " ${help}
+    exit 1
+fi
+
+# -o option check
+if [ -z ${outOpt_Key} ] || [ ${outOpt_Key} != "-o" ]; then
+    echo "[EROR] Specify '-g' as the third argument."
+    echo "      " ${help}
+    exit 1
+fi
+if [ -z ${genOpt_Val} ]; then
+    echo "[EROR] No value has been set for the '-g' option."
+    echo "      " ${help}
+    exit 1
+fi
+
+docker run --rm -v "${PWD}:/dest" openapitools/openapi-generator-cli generate -i ${inOpt_Val} -g ${genOpt_Val} -o ${outOpt_Val}
